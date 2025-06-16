7e5e9985864fe2375a970dbcbee1b2c3caf1b456
DevNap <thdwodh789@gmail.com>
2025-06-16 15:41:44 +0900

    [Feat] 에셋 추가

--- VR_Project/Assets/Scripts/GunController.cs
+++ VR_Project/Assets/Scripts/GunController.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class GunController : MonoBehaviour
 {
-    
+    [SerializeField] Transform _muzzlePoint;
+    [SerializeField] private GameObject _bullet;
+    [SerializeField] private float _bulletSpeed;
+
+    // Grab Interactable의 Activate 안에 들어가야 하기 때문에 public으로 만들어야 함.
+    public void Fire()
+    {
+        GameObject bullet = Instantiate(_bullet, _muzzlePoint.position, Quaternion.identity);
+        _bullet.GetComponent<Rigidbody>().AddForce(_muzzlePoint.forward * _bulletSpeed, ForceMode.Impulse);
+    }
 }
